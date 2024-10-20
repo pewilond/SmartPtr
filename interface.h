0c023da9c191029cea73611ccc11331f99052487
Alexey <qexx20052008@gmail.ru>
2024-10-20 22:01:42 +0300

    pp

--- interface.h
+++ interface.h
@@ -1,7 +1,15 @@
 #ifndef INTERFACE_H
 #define INTERFACE_H
 
+#include <string>
+
+int getValidatedInt(const std::string& prompt);
+
+template<typename SequenceType>
+void workWithSequence(const std::string& sequenceName);
+
 void run_interface();
+
 void runTestsAndDisplayResults();
 
 #endif // INTERFACE_H
